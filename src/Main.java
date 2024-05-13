5c9f6d71186ba7fa8e725e00dcad8f18fb935ccf
원지연 <dnjs229@gmail.com>
2024-05-13 18:03:02 +0900

    기능 A

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("첫 번째 커밋");
-        System.out.println("두 번째 커밋");
-        System.out.println("세 번째 커밋");
-        System.out.println("네 번째 커밋");
-        System.out.println("다섯 번째 커밋");
-        System.out.println("여섯 번째 커밋");
-        System.out.println("일곱 번째 커밋");
-        System.out.println("새로운 커밋2");
+        System.out.println("기능 A");
     }
 }
