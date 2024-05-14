2efc0abe22c9e8790110fa78aaaddebb7953bff6
원지연 <dnjs229@gmail.com>
2024-05-14 19:50:39 +0900

    기능 A 이슈

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("기능 A");
         System.out.println("기능 B");
+        System.out.println("기능 A 이슈");
     }
 }
