6875461eb672dcdffe00f68e49df05c8f3567dee
원지연 <dnjs229@gmail.com>
2024-06-04 12:54:38 +0900

    feat: 로그인 기능 A

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("기능 A");
-        System.out.println("기능 B");
-        System.out.println("기능 A 이슈");
-        System.out.println("기능 B 이슈");
-        System.out.println("기능 C 이슈");
-        System.out.println("기능 D 이슈");
-        System.out.println("기능 1 이슈");
-        System.out.println("기능 2 이슈");
-        System.out.println("#28");
-        System.out.println("#29");
-
-
-        System.out.println("기능 A");
-        System.out.println("기능 B");
-        System.out.println("기능 C");
+        System.out.println("로그인 기능 A 완료");
     }
 }
