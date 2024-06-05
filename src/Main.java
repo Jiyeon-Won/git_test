16e191382135d9a5ad5d187a345d58509fe9f191
원지연 <dnjs229@gmail.com>
2024-06-05 23:24:17 +0900

    feat: PR 첫번째 - 수정

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("로그인 기능 A 완료");
-        System.out.println("로그인 기능 B 완료");
         System.out.println("PR 첫번째");
     }
 }
