47a2637804960c5a87b110cac26af9a8a84f3be2
원지연 <dnjs229@gmail.com>
2024-06-05 23:21:58 +0900

    feat: PR 첫번째

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("로그인 기능 A 완료");
         System.out.println("로그인 기능 B 완료");
-        System.out.println("asdf");
+        System.out.println("PR 첫번째");
     }
 }
