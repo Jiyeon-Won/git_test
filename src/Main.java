8f3252d84cce1da45c7183f3286419816b1a1b4a
원지연 <dnjs229@gmail.com>
2024-06-18 23:26:23 +0900

    feat: update

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("PR 첫번째");
         System.out.println("create");
+        System.out.println("update");
     }
 }
